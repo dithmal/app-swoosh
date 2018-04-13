405639d38761d904f31d75f66743bcefc813b6de
Nadila Dithmal Kulanatha <nadiladithmal@gmail.com>
2018-04-13 21:29:07 +0530

    Created user interface

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Nadila Dithmal on 4/13/18.
+//  Copyright © 2018 Nadila Dithmal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
